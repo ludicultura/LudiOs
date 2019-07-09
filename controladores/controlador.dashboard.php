232d060ce1080d1101cd10542389ef0c6d084de2
RobertoVz <roberto@opalo.studio>
2019-07-09 15:09:08 -0500

    listo ahora si el merge

--- controladores/controlador.dashboard.php
+++ controladores/controlador.dashboard.php
@@ -1,16 +1,8 @@
 <script>
-<<<<<<< HEAD
-
-function cambiarDatos(){
-    $('#nombreLudi').html("Alberto");
-}
-
-=======
     $(document).ready(function() {
         $("#idDashboardNombre").text("<?php echo $_SESSION["sessionNombre"]; ?>");
         $("#idDashboardTipo").append("<?php echo $_SESSION["sessionTipo"]; ?>");
         $("#idDashboardComision").append("<?php echo $_SESSION["sessionComision"]; ?>");
         $("#idDashboardCarrera").append("<?php echo $_SESSION["sessionCarrera"]; ?>");
     });
->>>>>>> Alberto
 </script>
